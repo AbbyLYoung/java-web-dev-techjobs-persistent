e7f229ff23a03d5e5667f18682bbfda11d7ff844
AbbyLYoung <58016815+AbbyLYoung@users.noreply.github.com>
2020-03-08 16:13:01 -0500

    Part 2 + Employer Controler

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,5 +1,21 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+
+@Entity
 public class Skill extends AbstractEntity {
 
+    private String additionalSkillDescription;
+
+    public Skill(){
+
+    }
+
+    public String getAdditionalSkillDescription() {
+        return additionalSkillDescription;
+    }
+
+    public void setAdditionalSkillDescription(String additionalSkillDiscription) {
+        this.additionalSkillDescription = additionalSkillDiscription;
+    }
 }
